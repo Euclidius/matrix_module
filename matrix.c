670439265a78d88ce56569a20638c6033e0ba440
Evarist <perfodio@yandex.ru>
2023-03-04 23:04:01 +0300

    Added an ability to initalize and create a special matrixes

--- matrix.c
+++ matrix.c
@@ -35,6 +35,22 @@ void matrix_free(matrix *m) {
     free(m);
 }
 
+inline size_t matrix_get_rows(matrix *m) {
+    return m->rows;
+}
+
+inline size_t matrix_cget_rows(const matrix *m) {
+    return m->rows;
+}
+
+inline size_t matrix_get_cols(matrix *m) {
+    return m->cols;
+}
+
+inline size_t matrix_cget_cols(const matrix *m) {
+    return m->cols;
+}
+
 matrix *matrix_clone(const matrix *m) {
     matrix* new = matrix_alloc(m->cols, m->rows);
     if (new == NULL) {
@@ -55,14 +71,70 @@ matrix *matrix_resize(matrix *m, size_t h, size_t w) {
     }
 }
 
-double *matrix_get(matrix *m, size_t i, size_t j) {
+inline double *matrix_get(matrix *m, size_t i, size_t j) {
     return m->data + i * m->rows + j;
 }
 
-const double *matrix_cget(const matrix *m, size_t i, size_t j) {
+inline const double *matrix_cget(const matrix *m, size_t i, size_t j) {
     return m->data + i * m->rows + j;
 }
 
-void *matrix_set(matrix *m, size_t i, size_t j, double value) {
+void *matrix_sset(matrix *m, size_t i, size_t j, double value) {
+    if (matrix_get_rows(m) > i || matrix_get_cols(m) > j) {
+        return NULL;
+    }
     *(m->data + i * m->rows + j) = value;
+}
+
+matrix *matrix_set_zero(matrix *m) {
+    for (int i = 0; i < matrix_get_rows(m); ++i) {
+        for (int j = 0; j < matrix_get_cols(m); ++j) {
+            matrix_sset(m, i, j, 0);
+        }
+    }
+    return m;
+}
+
+matrix *matrix_alloc_zero(size_t h, size_t w) {
+    matrix *m = matrix_alloc(h, w);
+    if (m == NULL) {
+        return NULL;
+    }
+    return matrix_set_zero(m);
+}
+
+matrix *matrix_set_sqr_one(matrix *m) {
+    if (matrix_get_rows(m) != matrix_get_cols(m)) {
+        return NULL;
+    }
+    for (int i = 0; i < matrix_get_rows(m); ++i) {
+        for (int j = 0; j < matrix_get_cols(m); ++j) {
+            if (i == j) {
+                matrix_sset(m, i, j, 1);
+            } else {
+                matrix_sset(m, i, j, 0);
+            }
+        }
+    }
+    return m;
+}
+
+matrix *matrix_alloc_sqr_one(size_t h) {
+    matrix *m = matrix_alloc(h, h);
+    if (m == NULL) {
+        return NULL;
+    }
+    return matrix_set_sqr_one(m);
+}
+
+matrix *matrix_assign(matrix *m1, const matrix *m2) {
+    if (matrix_get_rows(m1) != matrix_cget_rows(m2) || matrix_get_cols(m1) != matrix_cget_cols(m2)) {
+        return NULL;
+    }
+    for (int i = 0; i < matrix_cget_rows(m2); ++i) {
+        for (int j = 0; j < matrix_cget_cols(m2); ++j) {
+            matrix_sset(m1, i, j, *matrix_cget(m2, i, j));
+        }
+    }
+    return m1;
 }
--- matrix.h
+++ matrix.h
@@ -13,6 +13,34 @@
 typedef struct matrix matrix;
 
 /**
+ * @brief returns a rows of a matrix
+ * @param m - pointer to an initialized matrix
+ * @return number of a rows
+ */
+size_t matrix_get_rows(matrix *m);
+
+/**
+ * @brief returns a rows of a const matrix
+ * @param m - pointer to an initialized const matrix
+ * @return number of a rows
+ */
+size_t matrix_cget_rows(const matrix *m);
+
+/**
+ * @brief returns a cols of a matrix
+ * @param m - pointer to an initialized matrix
+ * @return number of a cols
+ */
+size_t matrix_get_cols(matrix *m);
+
+/**
+ * @brief returns a cols of a const matrix
+ * @param m - pointer to an initialized const matrix
+ * @return number of a cols
+ */
+size_t matrix_cget_cols(const matrix *m);
+
+/**
  * @brief allocates the memory for matrix
  * @param h - rows in matrix
  * @param w - columns in matrix
@@ -63,12 +91,58 @@ double *matrix_get(matrix *m, size_t i, size_t j);
 const double *matrix_cget(const matrix *m, size_t i, size_t j);
 
 /**
- * @brief sets the element of matrix
+ * @brief safely sets the element of matrix
  * @param m - pointer to the initialised matrix
  * @param i - row of the element
  * @param j - column of the element
- * @param value - an inserting value to matrix
+ * @param value - an inserting value to matrix or
+ *        NULL if inserting is impossible
  */
-void *matrix_set(matrix *m, size_t i, size_t j, double value);
+void *matrix_sset(matrix *m, size_t i, size_t j, double value);
+
+/**
+ * @brief fills the matrix with zeros
+ * @param m - pointer to initialized matrix
+ * @return this matrix filled by zeros
+ */
+matrix *matrix_set_zero(matrix *m);
+
+/**
+ * @brief allocates and fills the matrix with zeros
+ * @param h - rows of the matrix
+ * @param w - columns of the matrix
+ * @return pointer to zero-matrix or
+ *         NULL if allocation was failed
+ *
+ *@note need a matrix_free call
+ */
+matrix *matrix_alloc_zero(size_t h, size_t w);
+
+/**
+ * @brief fills the matrix by zeros with ones on the main diagonal
+ * @param m - pointer to initialized matrix
+ * @return this matrix filled by zeros with ones on the main diagonal
+ *         or NULL if matrix is non-square
+ */
+matrix *matrix_set_sqr_one(matrix *m);
+
+/**
+ * @brief allocates and fills the matrix with zeros with ones on the main diagonal
+ * @param h - rows of the matrix
+ * @return pointer to zero-matrix with ones on the main diagonal
+ *         or NULL if matrix is non-square
+ *
+ *@note need a matrix_free call
+ */
+matrix *matrix_alloc_sqr_one(size_t h);
+
+/**
+ * @brief assigns m2 to m1
+ * @param m1 - pointer to matrix from which assigns
+ * @param m2 - pointer to matrix to which assigns
+ * @return pointer to m1 or
+ *         NULL if assignment is impossible (matrixes have different sizes)
+ */
+matrix *matrix_assign(matrix *m1, const matrix *m2);
 
 #endif //INC_23_MATRIX_H
